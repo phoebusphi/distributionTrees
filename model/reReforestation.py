daaf8b18b06801075f6f885a2a0cda1180a86b86
F_Moreno <fermorenogom@gmail.com>
2022-11-04 11:55:21 -0600

    modificación de función de cruza

--- model/reReforestation.py
+++ model/reReforestation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from copy import deepcopy
+
 
 # Omega es la probabilidad de cruza
 # Mu es la probabilidad de mutation
@@ -8,6 +8,10 @@ from copy import deepcopy
 # arboles ->      | d_{1_{a_1}} ... d_{1_{a_1}}
 # delegaciones ^  |         .....
 #                 | d_{20_{a_1}} ... d_{20_{a_1}}
+
+
+def best(areaTree):
+    return np.max(areaTree), np.argmax(areaTree)
 
 
 class reReforestation:
@@ -23,7 +27,7 @@ class reReforestation:
         return np.sum(trees.reshape(16, 10), axis=0)
 
     def population(self) -> np.ndarray:
-        return np.random.randint(500,1000,(self.pop_size, self.cols, self.rows))
+        return np.random.randint(500, 1000, (self.pop_size, self.cols, self.rows))
 
     def mutation(self, pop):
         pop_tmp = pop.copy()
@@ -33,13 +37,12 @@ class reReforestation:
                 pop_tmp[ind][town][tree] = np.random.randint(0, 1000)
         return pop_tmp
 
-    def sumArea(self, trees_tmp) -> np.ndarray:
+    def sum_area(self, trees_tmp) -> np.ndarray:
         trees_area = trees_tmp.copy()
         area = np.array([np.random.uniform(1, 3) for _ in range(10)])
         for town in range(self.rows):
             trees_area[town, :] = area * trees_area[town, :]
         return np.sum(trees_area)
-
 
     def tournament(self, pop) -> list:
         pop_tmp = pop.copy()
@@ -70,62 +73,60 @@ class reReforestation:
             result.append(winner)
         return result
 
-
     def cross(self, father, mother) -> tuple:
-        row, col = father.shape
-        childO = np.zeros((row, col))
-        childT = np.zeros((row, col))
+        child_o = np.zeros((self.rows, self.cols))
+        child_t = np.zeros((self.rows, self.cols))
         for r in range(self.rows):
-            childO[r, 0:self.cols // 2] = father[r, 0:col // 2]
-            childO[r, self.cols // 2:self.cols] = mother[r, self.col // 2:col]
-            childT[r, 0:self.cols // 2] = mother[r, 0:self.cols // 2]
-            childT[r, self.cols // 2:self.cols] = father[r, self.cols // 2:self.cols]
-        return childO, childT
-
+            child_o[r, 0:self.cols // 2] = father[r, 0:self.cols // 2]
+            child_o[r, self.cols // 2:self.cols] = mother[r, self.cols // 2:self.cols]
+            child_t[r, 0:self.cols // 2] = mother[r, 0:self.cols // 2]
+            child_t[r, self.cols // 2:self.cols] = father[r, self.cols // 2:self.cols]
+        return child_o, child_t
 
     def selection(self, pop, best):
         pop_cross = pop.copy()
         cross_tmp = np.zeros((self.pop_size, self.rows, self.cols))
         cross_tmp[0] = best
-        father = tournament(pop_cross)
-        mother = tournament(pop_cross)
+        father = self.tournament(pop_cross)
+        mother = self.tournament(pop_cross)
         index = 1
+        for f, m in zip(father, mother):
+            pop_new = self.cross(f, m) if np.random.random() < self.omega else [f, m]
+            cross_tmp = self.fill_pop(index=index, cross_tmp=cross_tmp, pop_new=pop_new)
 
-        for i in range(self.pop_size // 4):
-            if np.random.random() < self.omega:
-                chil1, child2 = cross(father[i], mother[i])
-                cross_tmp[index] = chil1
-                cross_tmp[index+1] = chil2
-            else:
-                cross_tmp[index] = father[i]
-                cross_tmp[index+1] = mother[i]
-            index += 2
-        while len(cross_tmp) > 100:
-            cross_tmp.pop()
+#        for i in range(self.pop_size // 4):
+#            if np.random.random() < self.omega:
+#                child1, child2 = self.cross(father[i], mother[i])
+#                cross_tmp[index] = child1
+#                cross_tmp[index + 1] = child2
+#            else:
+#                cross_tmp[index] = father[i]
+#                cross_tmp[index + 1] = mother[i]
+#            index += 2
         return cross_tmp
 
+    def fill_pop(self, index, cross_tmp, pop_new) -> np.ndarray:
+        index_0 = index
+        while index < self.pop_size:
+            cross_tmp[index] = pop_new[index - index_0]
+            index += 1
+        return cross_tmp
 
-def penalty(pop, epsilon):
-    n = len(pop)
-    area = np.zeros(n)
-    for i in range(n):
-        ic(type(pop[i]))
-        area[i] = sumArea(pop[i])
-    area[area > epsilon] = area[area > epsilon] * (np.random.uniform(0.5, 0.7))
-    return area
+    def penalty(self, pop, epsilon):
+        n = len(pop)
+        area = np.zeros(n)
+        for i in range(n):
+            area[i] = self.sum_area(pop[i])
+        area[area > epsilon] = area[area > epsilon] * (np.random.uniform(0.5, 0.7))
+        return area
 
-
-def best(areaTree):
-    return (np.max(areaTree), np.argmax(areaTree))
-
-
-def main(epsilon=100000):
-    indBest = {}
-    poP = population()
-    for i in range(100):
-        print('generacion ', i)
-        popPenalty = penalty(poP, epsilon)
-        bestInd = best(popPenalty)
-        indBest[bestInd[0]] = popPenalty[bestInd[1]]
-        crossPop = selection(poP, poP[bestInd[1]])
-        poP = mutation(crossPop)
+    def main(self, epsilon=100000):
+        ind_best = {}
+        pop_ = self.population()
+        for i in range(100):
+            print('generacion ', i)
+            pop_penalty = self.penalty(pop_, epsilon)
+            best_ = best(pop_penalty)
+            ind_best[best_[0]] = pop_penalty[best_[1]]
+            cross_pop = self.selection(pop_, pop_[best_[1]])
+            pop_ = self.mutation(cross_pop)
